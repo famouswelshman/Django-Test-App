ca38c9fcb07283c8ab1c3687b23034e0be2b5591
James Hunt <famouswelshman@yahoo.co.uk>
2022-03-17 11:25:27 +0000

    added pages

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,4 +7,7 @@ def index(response):
     return HttpResponse("Tech with James!")
 
 def v1(response):
-    retunr HttpResponse("<h1>view 1</h1>")
+    return HttpResponse("view 1")
+
+def main(response):
+    return HttpResponse("main")
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1', include("main.urls")),
+    path('', include("main.urls")),
 ]
 
 
